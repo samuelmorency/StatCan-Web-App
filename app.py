4737cbaf087face86f65a6931cab6c11ba814bd4
samuelmorencyIIC <samuel.morency@invcanada.ca>
2024-12-02 15:49:55 -0500

    fixed app.layout

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
-app.layout = [html.Div(children='Hello World')]
+app.layout = html.Div(children='Hello World')
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
