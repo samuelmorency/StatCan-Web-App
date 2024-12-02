e394f7034ef5f5abc0d3cecdf4e518518dbc34b1
samuelmorencyIIC <samuel.morency@invcanada.ca>
2024-12-02 13:06:49 -0500

    Simplified to hello world

--- app.py
+++ app.py
@@ -1,28 +1,16 @@
-from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
-import pandas as pd
+from dash import Dash, html
 from flask import Flask
 import dash_bootstrap_components as dbc
 
 flask_server = Flask(__name__)
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
-
 app = Dash(__name__, server=flask_server, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = [
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
-]
+app.layout = [html.Div(children='Hello World')]
 
-@callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
-)
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
-
+# main driver function
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
