b1fe86a3308c7c3c6fdd292f2ff243da3508caeb
samuelmorencyIIC <samuel.morency@invcanada.ca>
2025-01-23 11:58:05 -0500

    Update app layout: Change background color and improve table content structure for better clarity

--- app_layout.py
+++ app_layout.py
@@ -274,22 +274,19 @@ def create_layout(data, stem_bhase_options_full, year_options_full, prov_options
                     dbc.Col([province_card], width=6)
                 ], className="mb-4"),
             ])
-        ], style={'background-color': '#d5dadc'})
+        ], style={'background-color': '#F1F1F1'})
     ])
 
-    table_content = dbc.Card([
-        dbc.CardHeader(
-            dbc.Row([
-                dbc.Col(html.H3("Graduate Data Pivot Table"), width=9),
-                dbc.Col(html.Button("Export to CSV", **download_button_args), width=3),
-            ]),
-            className="d-flex align-items-center"
-        ),
-        dbc.CardBody([
+    table_content = dbc.Row([
+        dbc.Card([
+            dbc.Stack([
+                dbc.Col(html.H3("Graduate Data Pivot Table")),
+                dbc.Col(dbc.Button("Download as Displayed", id='download-button', color="secondary", className="ms-auto")),
+            ], direction="horizontal", className="mb-4"),            
             dcc.Download(id="download-data"),
             initialize_pivot_table(data)  # Pass the dataset to pivot table
-        ])
-    ], className="mb-4")
+        ], className="m-4")
+    ], style={'background-color': '#F1F1F1'})
 
     # Create the app layout with tabs
     app_layout = html.Div([
@@ -306,7 +303,7 @@ def create_layout(data, stem_bhase_options_full, year_options_full, prov_options
                 width=12)
             ]),
             dbc.Tabs([
-                dbc.Tab(visualization_content, label="Interactive Map and Charts", tab_id="tab-visualization", active_label_class_name="bg-secondary text-white"),
+                dbc.Tab(visualization_content, label="Interactive Map and Charts", tab_id="tab-visualization", active_label_class_name="bg-light text-black"),
                 dbc.Tab(table_content, label="Data Explorer", tab_id="tab-data", active_label_class_name="bg-secondary text-white"),
             ], id="tabs", active_tab="tab-visualization"),
             ], fluid=True)
